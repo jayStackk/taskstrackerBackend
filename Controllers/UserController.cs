52b553221b9481d9a2d35e5cb269814fd279352a
jayStackk <jmagpantay@codestack.co>
2023-04-24 11:38:59 -0700

    NewBranch

---
+++ Controllers/UserController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace tasktrackerBackend.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UserController : ControllerBase
+{
+ 
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+
+using Microsoft.EntityFrameworkCore;
+
+
+
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<UserService>();
+builder.Services.AddScoped<BlogService>();
+builder.Services.AddScoped<PasswordService>();
+
+var connectionString = builder.Configuration.GetConnectionString("MyBlogString");
+
+
+builder.Services.AddCors(options => {
+    options.AddPolicy("BlogPolicy", 
+    builder => {
+        builder.WithOrigins("http://localhost:3000")
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
+
+// Add services to the container.
+
+
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// app.UseHttpsRedirection();
+app.UseCors("BlogPolicy");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
