51f5e47c1223a79fb7cb1b350b3f1327f77d45b9
DanDecoito <116022076+DanDecoito@users.noreply.github.com>
2023-04-25 09:46:45 -0700

    DanBranch Commit

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,10 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using system.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using tasktrackerBackend.Models.DTO;
 
-namespace tasktrackerBackend.Controllers;
-
-[ApiController]
-[Route("[controller]")]
-public class UserController : ControllerBase
+namespace tasktrackerBackend.controllers
 {
- 
+
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {  
+         // Login
+         private readonly UserService _data;
+         public UserController(){}
+
+        public bool AddUser(CreateAccountDTO UserToAdd)
+        {
+           
+        }
+     
+        // If the user already exist
+        // If the user does not exist, create a new user
+        // Else throw a false
+
+
+
+
+
+
+    }
 }
---
+++ Services/UserServices.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using tasktrackerBackend.Models.DTO;
+
+
+namespace tasktrackerBackend.Services
+{
+    public class UserService
+    {
+        public bool AddUser(CreateAccountDTO UserToAdd)
+        {
+
+        }
+
+    }
+
+}
