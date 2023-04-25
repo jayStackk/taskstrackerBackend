03abda03e7f82fa64162e7b38c6c8dd50ca454d1
Lerissa Lazar <lerissa.lazar12@gmail.com>
2023-04-25 10:41:14 -0700

    models update

---
+++ Models/UserModel.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tasktrackerBackend.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+
+        public string? Username { get; set; }
+
+        public string? Salt { get; set; }
+
+        public string? Hash { get; set; }
+
+        public bool isAdmin { get; set; }
+
+        public UserModel(){
+
+        }
+    }
+}
