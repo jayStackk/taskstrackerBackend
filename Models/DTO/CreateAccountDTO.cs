ecbc11505f457731e7d38e1e5573ab4f3b19c3a2
Lerissa Lazar <lerissa.lazar12@gmail.com>
2023-04-26 09:47:01 -0700

    update

--- Models/DTO/CreateAccountDTO.cs
+++ Models/DTO/CreateAccountDTO.cs
@@ -8,8 +8,8 @@ namespace tasktrackerBackend.Models.DTO
     public class CreateAccountDTO
     {
         public int Id { get; set; } 
-        public string? UserName { get; set;}
-        public string? Password { get; set;}
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
 
     }
 }
