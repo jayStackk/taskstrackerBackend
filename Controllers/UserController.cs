f4f09496ddcd7b2998985a38f788445272b88b7d
DanDecoito <116022076+DanDecoito@users.noreply.github.com>
2023-04-27 10:01:39 -0700

    UserServices/Controller Fin

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -12,26 +12,65 @@ namespace tasktrackerBackend.controllers
     [ApiController]
     [Route("[controller]")]
     public class UserController : ControllerBase
-    {  
-         // Login
-         private readonly UserService _data;
-         public UserController(UserService dataFromService)
-         {
+    {
+        // Login
+        private readonly UserService _data;
+        public UserController(UserService dataFromService)
+        {
             _data = dataFromService;
-         }
+        }
 
+
+        [HttpPost]
+        [Route("Login")]
+        public IActionResult Login(LoginDTO User)
+        {
+            return _data.Login(User);
+        }
+
+
+        [HttpPost]
+        [Route("AddUser")]
         public bool AddUser(CreateAccountDTO UserToAdd)
         {
-           return _data.AddUser(UserToAdd);
+            return _data.AddUser(UserToAdd);
         }
-     
+
         // If the user already exist
         // If the user does not exist, create a new user
         // Else throw a false
 
 
+        [HttpPost]
+        [Route("UpdateUser")]
+        public bool UpdateUser(UserModel userToUpdate)
+        {
+            return _data.UpdateUser(userToUpdate);
+        }
 
 
+        [HttpDelete]
+        [Route("DeleteUser/{userToDelte}")]
+        public bool DeleteUser(string userToDelte)
+        {
+            return _data.DeleteUser(userToDelte);
+        }
+
+
+        [HttpPost]
+        [Route("UpdateUser/{id}/{username}")]
+        public bool UpdateUser(int id, string username)
+        {
+            return _data.UpdateUsername(id, username);
+        }
+
+
+        [HttpGet]
+        [Route("userByUsername/{username}")]
+        public UserIdDTO GetUserIdDTOByUsername(string username)
+        {
+            return _data.GetUserIdDTOByUsername(username);
+        }
 
 
     }
